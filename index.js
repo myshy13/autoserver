7afc34c8e473eca58d7b6b39eb3ae2e2d52c3502
myshy13 <hamish.oliver@icloud.com>
2024-11-24 15:12:15 +1100

    no commit msg

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const { exec } = require('child_process');
+// cli.js
+const { Command } = require('commander');
+const program = new Command();
 
-console.log("_________________");
-console.log("|               |");
-console.log("|               |");
-console.log("|               |");
-console.log("|_______________|");
+program
+  .version('1.0.0')
+  .description('the autoscrip[t  cli tool');
+
+program
+  .command("run <file>")
+  .description("run an autoscript file")
+  .option("-v, --verbose", "displays more info")
+  .action(() => {console.log("hello")})
+
+  program.parse(process.argv);
