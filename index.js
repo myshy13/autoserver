5213426530d4b6f525f0e840311388b5c5994eea
myshy13 <hamish.oliver@icloud.com>
2024-11-24 15:09:49 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const { exec } = require('child_process');
+
+console.log("_________________");
+console.log("|               |");
+console.log("|               |");
+console.log("|               |");
+console.log("|_______________|");
